e075fd7a8eae381501b00151b235df6a3a57f7f9
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-02-26 22:37:17 +0100

    train method

--- train.py
+++ train.py
@@ -1,20 +1,15 @@
-from dataset import CIFAR10GAN
+from dataset import download_dataset
 import torch
-from models import DiscriminatorCIFAR10, GeneratorCIFAR10
 from torch.optim import Adam
 from torch.nn import BCELoss
 import logging
-from torchvision import transforms
-from torchvision.utils import save_image
-from mat_data_utils import construct_loader
+
 
 def train_model(
         device, 
         n_epochs: int,
         dataset_name: str,
         batch_size: int,
-        download_datasets: bool,
-        root_datasets_dir: str,
         model_type: str
     ):
     '''
@@ -24,23 +19,13 @@ def train_model(
     device 
     n_epochs: int
         number of training epochs
+    dataset_name: str
+        Name of dataset available through MoleculeNet
     batch_size: int
         number of images inside single batch
-    ref_images_dir: str
-        path to directory where ref images will be stored
-    download_datasets: bool
-        True -> download dataset from torchvision repo
-    root_datasets_dir: str
-        path to directory where dataset should be downloaded (download_datasets = True)
-        or where dataset is already stored
-    class_name: str
-        one of ten classes in CIFAR10 dataset
-    latent_vector_length: int
-        length of random vector which will be transformed into an image by generator
-    init_generator_weights: bool
-        Init generator's weights using normal distribiution
-    init_discriminator_weights: bool
-        Init discriminator's weights using normal distribiution
+    model_type: str
+        type of model which will be trained
     '''
 
-
+    # datasets and dataloaders
+    train_dataset, test_dataset = download_dataset("HIV", "ECFP", True)
