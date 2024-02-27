9cf8189c3870b1ba5f2443d7905f7f90d28ad33e
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-02-27 17:46:22 +0100

    main

--- main.py
+++ main.py
@@ -47,4 +47,4 @@ if __name__ == "__main__":
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     logging.info(f"Device: {device}")
 
-    model = train_model(device, args["n_epochs"], args["batch_size"], args["dataset_name"], args["batch_size"], args["model_type"])
+    model = train_model(device, args["n_epochs"], args["dataset_name"], args["batch_size"], args["model_type"])
