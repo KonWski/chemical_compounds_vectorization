156f4f3defe318316e60637831ff862f6f63dd6e
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-03-05 18:19:02 +0100

    MoleculeDataLoader

--- dataset.py
+++ dataset.py
@@ -5,6 +5,12 @@ from torch.utils.data import Dataset, Sampler
 import os
 import pickle
 from torch.utils.data import DataLoader
+from typing import Any, Callable, Iterable, TypeVar, List, Optional, Union
+
+T_co = TypeVar('T_co', covariant=True)
+T = TypeVar('T')
+_worker_init_fn_t = Callable[[int], None]
+_collate_fn_t = Callable[[List[T]], Any]
 
 class MoleculeDataset(Dataset):
 
@@ -176,15 +182,30 @@ class MoleculeDataset(Dataset):
 
 class MoleculeDataLoader(DataLoader):
 
-    def __init__(self, dataset, batch_size, shuffle, sampler, batch_sampler, num_workers, collate_fn, pin_memory, drop_last, 
-                 timeout, worker_init_fn, multiprocessing_context, generator, prefetch_factor, persistent_workers):
-
-        if dataset.prepare_data_for_mat and collate_fn is None:
-            collate_fn = self._collate_extra_features
-
+    def __init__(self,
+                 dataset: Dataset[T_co], 
+                 batch_size: Optional[int] = 1,
+                 shuffle: Optional[bool] = None, 
+                 sampler: Union[Sampler, Iterable, None] = None,
+                 batch_sampler: Union[Sampler[List], Iterable[List], None] = None,
+                 num_workers: int = 0, 
+                 collate_fn: Optional[_collate_fn_t] = None,
+                 pin_memory: bool = False, 
+                 drop_last: bool = False,
+                 timeout: float = 0, 
+                 worker_init_fn: Optional[_worker_init_fn_t] = None,
+                 multiprocessing_context=None, 
+                 generator=None,
+                 *, 
+                 prefetch_factor: Optional[int] = None,
+                 persistent_workers: bool = False,
+                 pin_memory_device: str = ""):
 
         super().__init__(dataset, batch_size, shuffle, sampler, batch_sampler, num_workers, collate_fn, pin_memory, drop_last, 
                          timeout, worker_init_fn, multiprocessing_context, generator, prefetch_factor, persistent_workers)
+
+        if dataset.prepare_data_for_mat and collate_fn is None:
+            self.collate_fn = self._collate_extra_features
 
     
 
