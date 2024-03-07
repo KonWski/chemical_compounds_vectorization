862eeff00eb75abfaea30f1b7922d72f5e2404f3
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-03-07 20:23:13 +0100

    dataset

--- dataset.py
+++ dataset.py
@@ -123,16 +123,16 @@ class MoleculeDataset(Dataset):
                     pickle.dump(distance_matrices, fp)
 
         # convert outputs to tensors
-        print(f"node_features[0].shape: {node_features[0].shape}")
-        print(f"node_features[1].shape: {node_features[1].shape}")
+        # print(f"node_features[0].shape: {node_features[0].shape}")
+        # print(f"node_features[1].shape: {node_features[1].shape}")
 
-        print(f"np.array(node_features).shape: {np.array(node_features).shape}")
-        print(f"np.array(adjacency_matrices).shape: {np.array(adjacency_matrices).shape}")
-        print(f"np.array(distance_matrices).shape: {np.array(distance_matrices).shape}")
+        # print(f"np.array(node_features).shape: {np.array(node_features).shape}")
+        # print(f"np.array(adjacency_matrices).shape: {np.array(adjacency_matrices).shape}")
+        # print(f"np.array(distance_matrices).shape: {np.array(distance_matrices).shape}")
 
-        node_features = torch.Tensor(np.array(node_features)) 
-        adjacency_matrices = torch.Tensor(np.array(adjacency_matrices)) 
-        distance_matrices = torch.Tensor(np.array(distance_matrices)) 
+        node_features = torch.Tensor(np.stack(node_features, axis=0)) 
+        adjacency_matrices = torch.Tensor(np.stack(adjacency_matrices, axis=0)) 
+        distance_matrices = torch.Tensor(np.stack(distance_matrices, axis=0)) 
 
         return node_features, adjacency_matrices, distance_matrices
 
