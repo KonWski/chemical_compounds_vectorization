e13c4a5134952cda4088701c3fc00c27107eb75e
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2024-03-19 19:28:40 +0100

    auc

--- mat/mat_train.py
+++ mat/mat_train.py
@@ -113,8 +113,8 @@ def train_mat(
                     if trainset.prediction_task == "classification":
                         outputs = torch.sigmoid(outputs)
 
-                    # print(f"outputs: {outputs}")
-                    # print(f"labels: {labels.cpu().detach().numpy()}")
+                    print(f"outputs: {outputs.cpu().detach().numpy()}")
+                    print(f"labels: {labels.cpu().detach().numpy()}")
                     loss = criterion(outputs.cpu().detach().numpy(), labels.cpu().detach().numpy())
 
                     if state == "train":
